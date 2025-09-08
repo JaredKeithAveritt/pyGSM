a31a29910bb6ae4dcd905ade4cff7be9121893e4
Jared Keith Averitt <103066434+JaredKeithAveritt@users.noreply.github.com>
2025-09-08 09:25:34 -0600

    Update run.sh

--- DE-GSM/run.sh
+++ DE-GSM/run.sh
@@ -1,7 +1,6 @@
-
-gsm  -xyzfile molecule.xyz  \
-    -mode DE_GSM \
-    -nnodes 1 \
-    -package Gaussian \
-    -lot_inp_file gstart \
+gsm -xyzfile molecule.xyz `
+    -mode DE_GSM `
+    -num_nodes 1 `
+    -package Gaussian `
+    -lot_inp_file gstart `
     -coordinate_type DLC > log 2>&1
